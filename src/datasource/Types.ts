2fa14219f6c5d31404a1d364c8d6e471969bbfa8
lin <2277422066@qq.com>
2021-03-17 17:10:33 +0800

    init

---
+++ src/datasource/Types.ts
@@ -0,0 +1,42 @@
+export interface Course {
+    id?: number;
+    name?: string;
+    createTime?: string;
+  }
+  
+  export interface User {
+    id?: number;
+    level?: number;
+    name?: string;
+    address?: string;
+    insertTime?: string;
+    sex?: string;
+    title?: Title;
+    courses?: Course[];
+  }
+  
+  export interface Title {
+    id?: number;
+    name?: string;
+  }
+  
+  export interface GithubUser {
+    name?: string;
+    company?: string;
+    public_repos?: number;
+    followers?: number;
+    repos_url?: string;
+    login?: string;
+    avatar_url?: string;
+    url?: string;
+  }
+  
+  export interface GithubRepos {
+    name?: string;
+    html_url?: string;
+    updated_at?: string;
+    language?: string;
+    size?: number;
+    forks?: number;
+    stargazers_count?: number;
+  }
---
+++ src/views/example03/Example03.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <p>
+      <b>一定在router配置声明启用props支持，否则无法获取数据。</b>
+      <br />
+      在vue props属性中声明同名变量
+    </p>
+    studentID: {{ sid }}
+    <br />
+    homeworksID: {{ hid }}
+    <br />
+    也可通过$route.params调用
+    <br />
+    studentID: {{ $route.params.sid }}
+    <br />
+    homeworksID: {{ $route.params.hid }}
+    <hr />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: ["sid", "hid"],
+  data: () => ({
+    homeworks: [
+      { id: 1, title: "spring" },
+      { id: 2, title: "springMVC" },
+      { id: 3, title: "JPA" }
+    ]
+  }),
+  created() {
+    //组件首次创建时调用
+    //基于参数异步请求数据再渲染
+    console.log(this.$route.params.sid);
+    console.log(this.$route.params.hid);
+    this.init();
+  },
+  beforeUpdate() {
+    //组件数据变化时，例如props改变时调用
+    this.init();
+  },
+  beforeRouteUpdate(to, from, next) {
+    //复用组件渲染后，路由更新前（首次进入不会回调）回调。可替代beforeUpdate()
+    this.init();
+  },
+  methods: {
+    init() {
+      console.log("init()");
+    }
+  }
+});
+</script>
